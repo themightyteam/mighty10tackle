c7f7642e2aa28bf8f1911071c3c656615ad06447
hector <hcerezo@gradiant.org>
2016-04-17 23:40:47 +0200

    Small changes in map

--- core/src/ai/decision/StatePropOnlyEnemyAttack.java
+++ core/src/ai/decision/StatePropOnlyEnemyAttack.java
@@ -5,11 +5,11 @@ public class StatePropOnlyEnemyAttack extends StateProp
 
 	public StatePropOnlyEnemyAttack()
 	{
-		this.propGoToOtherBase = 0.05;
-		this.propGoToMyBase = 0.3;
-		this.propGoToRandomSquareInZone = 0.35;
-		this.propGoToRandomSquare = 0.40;
-		this.propGoToClosestEnemy = 0.7;
+		this.propGoToOtherBase = 0.5;
+		this.propGoToMyBase = 0.7;
+		this.propGoToRandomSquareInZone = 0.7;
+		this.propGoToRandomSquare = 0.7;
+		this.propGoToClosestEnemy = 0.75;
 		this.propGoToClosestEnemyWithFlag = 1;
 		this.propGoToClosestTeammate = 1;
 		this.propGoToClosestTeamMateWithFlag = this.propGoToClosestTeammate;
